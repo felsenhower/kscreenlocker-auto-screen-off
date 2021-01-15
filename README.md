5d1dff44975ef55c83823b628dd6cd9378afdfda
Ruben Felgenhauer <4felgenh@informatik.uni-hamburg.de>
2021-01-15 12:10:07 +0100

    Add code and readme

---
+++ README.md
@@ -0,0 +1,72 @@
+# kscreenlocker-auto-screen-off
+
+This is a small Python script that I wrote because I was very annoyed with the
+fact that, at the time of writing, the KDE lockscreen doesn't support shutting
+off the screen automatically after `n` minutes of time have passed in which
+no input (e.g. keyboard / mouse) was detected (like you would expect a
+lockscreen to work).
+
+There is a [workaround](https://askubuntu.com/a/351713) that makes the
+screen immediately be turned off when the screen is locked. This is not
+ideal because if I wake up the screen and *don't unlock* my computer and walk
+away again, the screen will not be locked again, and of course the screen is
+also immediately turned off which I do not want.
+
+However, I'm using a part of the answer myself.
+
+## How It Works
+
+The script needs `xinput`, `xset`, `pgrep` and `kscreenlocker` to work.
+
+If you lock your screen and don't touch your mouse and keyboard for 5 minutes,
+the screen will automatically be turned off. If you move your mouse, your
+screen should wake up and you should see your lockscreen. If you repeat this
+and wait another 5 minutes, the screen will be turned off again, until you
+unlock your session.
+
+To do this, the script will…
+
+* acquire a list of all slave input devices by running `xinput --list --short`,
+* parse out the relevant device IDs,
+* for each ID, run `xinput --test $id` as a subprocess, and
+* merge all their stdout streams to one stream that has a timeout of one second.
+* If this timeout passes, this means, that no `xinput` instance has produced
+  any output in the last second, i.e. no input device has been used.
+* If however input is done, this is immediately detected and we update a timepoint
+  variable.
+* We measure the time since the last input.
+* If this time is larger than the configured timeout, we lock the screen using
+  `xset dpms force off`.
+* If the screen is not locked anymore, i.e. `kscreenlocker` is not running,
+  the script kills all subprocesses and exits.
+
+## How To Use
+
+Clone the repo whereever you want:
+```shell
+$ git clone https://github.com/felsenhower/kscreenlocker-auto-screen-off
+```
+
+Run the script normally if everything works normally.
+Output should look something like this:
+```
+$ cd kscreenlocker-auto-screen-off
+$ python3 ./auto-screen-off.py
+Found device IDs [4, 8, 9, 11, 5, 6, 7, 10, 12, 13, 14]
+Spawning 11 xinput subprocesses…
+Screen is not locked. Exiting…
+```
+
+Set the variable `timeout_s` in the script to your liking, default is 5 minutes.
+
+Link the Lock screen to the script. Here is how to do it in KDE Plasma 5.20.4:
+
+* Go to the System Settings
+* Scroll down to "Personalisation" → "🔔 Notifications"
+* Click on "Applications: ⚙️ Configure…" at the very bottom
+* Scroll down to "System Services" → "🔒 Screen Saver"
+* Click on "🗨️ Configure Events…"
+* Select the "Screen locked" event at the top
+* Activate the checkbox "☑ ⚙️ Run command"
+* Enter `/usr/bin/python3 "/path/to/your/auto-screen-off.py"`
+* Click "✔ OK"
---
+++ auto-screen-off.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+
+import subprocess
+import re
+import time
+import select
+import typing
+from typing import List, Any
+Process = Any
+
+
+timeout_s: int = 5 * 60    # Timeout in lockscreen in seconds, i.e. when to turn of screen
+poll_period_ms: int = 1    # How quickly to read lines from stdout in ms
+stream_timeout_s: int = 1  # How quickly to timeout stdout in seconds (to be able to check the time)
+initial_sleep_s: int = 1   # Initially sleep for this duration in seconds
+
+
+def check_system() -> bool:
+    """
+    Check if the system has all dependencies
+    :return: True if xinput, xset and pgrep can be found
+    """
+    ret = True
+    for app in ["xinput", "xset", "pgrep"]:
+        if (subprocess.run(["which", app], capture_output=True).returncode != 0):
+            ret = False
+            print("Application \"{}\" is missing!".format(app))
+    return ret
+
+
+def get_device_ids() -> List[int]:
+    """
+    Get the device IDs of all connected slave input devices via xinput
+    return: The list of device IDs as int
+    """
+    xinput_output = subprocess.check_output(["xinput", "--list", "--short"]) \
+                              .decode("utf-8").split("\n")
+    r = re.compile(r"id=(.*)\s*\[slave.*\]")
+    device_ids = []
+    for line in xinput_output:
+        if m := r.search(line):
+            device_id = int(m.group(1))
+            device_ids.append(device_id)
+    print("Found device IDs {}".format(device_ids))
+    return device_ids
+
+
+def spawn_xinput_subprocesses(device_ids: List[int]) -> List[Process]:
+    """
+    Spawn the xinput subprocesses that monitor each input device
+    :param device_ids: The device IDs to monitor
+    :return: The list of spawned xinput processes
+    """
+    subprocesses: List[Process] = [None for i in device_ids]
+    print("Spawning {} xinput subprocesses…".format(len(subprocesses)))
+    for i in range(len(subprocesses)):
+        id = device_ids[i]
+        subprocesses[i] = subprocess.Popen(["xinput", "--test", str(id)],
+                                           stdout=subprocess.PIPE,
+                                           stderr=subprocess.PIPE)
+    return subprocesses
+
+
+def is_screen_locked() -> bool:
+    """
+    Check if the screen is locked by checking if kscreenlocker is running
+    :return: True if kscreenlocker is running
+    """
+    return (subprocess.run(["pgrep", "kscreenlocker"], capture_output=True).returncode == 0)
+
+
+def turn_screen_off() -> None:
+    """
+    Turn the screen off via xset
+    """
+    print("Turning screen off…")
+    subprocess.run(["xset", "dpms", "force", "off"])
+
+
+def poll_for_input(subprocesses: List[Process]) -> None:
+    """
+    Measure the time since the last input on the system.
+    This is done by merging all output streams of the processes into one stream
+    with a timeout. If this timeout passes, no input was in this period.
+    Turn off the screen if the time > timeout_s.
+    Exit and kill all subprocesses if the screen is not locked.
+    Also exit if all subproccesses terminate for some reason.
+    :param subprocesses: The list of xinput subproccesses to monitor
+    """
+    time_of_last_input = time.time()
+    streams = [p.stdout for p in subprocesses]
+    while True:
+        if not is_screen_locked():
+            print("Screen is not locked. Exiting…")
+            for p in subprocesses:
+                if p != None:
+                    p.kill()
+            break
+        now = time.time()
+        time_since_last_input = now - time_of_last_input
+        if time_since_last_input > timeout_s:
+            print("Time since last input: {} s".format(time_since_last_input))
+            turn_screen_off()
+        rstreams, _, _ = select.select(streams, [], [], stream_timeout_s)
+        for stream in rstreams:
+            stream.readline()
+            time_of_last_input = time.time()
+        if all(p.poll() is not None for p in subprocesses):
+            print("Error: All subprocesses terminated! Exiting…")
+            break
+        time.sleep(poll_period_ms / 1000)
+
+
+def main() -> None:
+    if check_system():
+        time.sleep(initial_sleep_s)
+        device_ids = get_device_ids()
+        subprocesses = spawn_xinput_subprocesses(device_ids)
+        poll_for_input(subprocesses)
+
+
+if __name__ == '__main__':
+    main()
